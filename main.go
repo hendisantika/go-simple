d7a2a95e2d9d5b1be7da05e1f71c19359374e56b
hendisantika <hendisantika@yahoo.co.id>
2025-02-27 12:01:23 +0700

    feat: Add Greeting🫸🌀✏️📗🐧🐳⬆

--- main.go
+++ main.go
@@ -2,10 +2,42 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"path"
+	"strings"
 )
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+const (
+	Port = ":8080"
+)
+
+func Greeting(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "Buddy"
+	} else {
+		name = strings.Title(name)
+	}
+	var filepath = path.Join("", "index.html")
+	var tmpl, err = template.ParseFiles(filepath)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var data = map[string]interface{}{
+		"title": "Greeting",
+		"name":  name,
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
 func main() {
 	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
